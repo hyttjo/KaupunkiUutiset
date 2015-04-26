7a8ecfb6f1c90761bc8b667a809772cc2f95118c
hyttjo <jouni.ilmari.hyttinen@gmail.com>
2015-04-26 21:35:34 +0300

    Lisätty merkintä uutisen päivityksestä itse uutiseen

--- php/scripts/update_news.php
+++ php/scripts/update_news.php
@@ -5,8 +5,13 @@
     $summary = str_replace("'", "", $_POST['summary']);
     $news_text = str_replace("'", "", $_POST['news_text']);
     $news_id = $_POST['news_id'];
-    
-    $query = "UPDATE news SET date=DATE_ADD(now(), INTERVAL 9 HOUR), header='$header', summary='$summary', text='$news_text' WHERE id='$news_id'";
+
+    date_default_timezone_set('Europe/Helsinki');
+    $current_date = date('d/m/Y H:i:s');
+
+    $modified_text = '<b>Uutinen päivitetty: '. $current_date .'</b><br><br>' . $news_text;
+
+    $query = "UPDATE news SET header='$header', summary='$summary', text='$modified_text' WHERE id='$news_id'";
 
     if(mysqli_query($con, $query)) {
         echo "Olet päivittänyt uutisen onnistuneesti.";
